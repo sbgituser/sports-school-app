fdf40664fab497d001f7ca42dd05ba46e64a7209
sbgituser <ukuiuta58@gmail.com>
2025-04-28 15:10:27 +0900

    update css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  };
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  safelist: [
+    'bg-blue-700',
+    'text-white',
+    'hover:bg-blue-800',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
